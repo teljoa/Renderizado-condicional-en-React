d37c1678bbd082ef4641e206406e8a44773ab3fb
Joaquin Garcia <tellezjoa@gmail.com>
2025-10-22 12:51:15 +0200

    correciones

--- Boletin/src/App.jsx
+++ Boletin/src/App.jsx
@@ -11,31 +11,31 @@ function App() {
     <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
       <h1>Renderizado Condicional en React</h1>
 
-      <h2>Ejercicio 1: if/else</h2>
+      <h2>Ejercicio 1</h2>
       <MensajeBienvenida isLoggedIn={true} />
       <MensajeBienvenida isLoggedIn={false} />
 
-      <h2>Ejercicio 2: Ternario</h2>
+      <h2>Ejercicio 2</h2>
       <EstadoDeCuenta isPositive={true} />
       <EstadoDeCuenta isPositive={false} />
 
-      <h2>Ejercicio 3: &&</h2>
+      <h2>Ejercicio 3</h2>
       <AvisoError hasError={true} />
       <AvisoError hasError={false} />
 
-      <h2>Ejercicio 4: Ternario (✔️ / ❌)</h2>
+      <h2>Ejercicio 4</h2>
       <Notificacion isCompleted={true} />
       <Notificacion isCompleted={false} />
 
-      <h2>Ejercicio 5: null</h2>
+      <h2>Ejercicio 5</h2>
       <ListaDeTareas />
 
-      <h2>Ejercicio 6: Ternarios anidados</h2>
+      <h2>Ejercicio 6</h2>
       <EstadoConexion status="online" />
       <EstadoConexion status="offline" />
       <EstadoConexion status="away" />
 
-      <h2>Ejercicio 7: Variable condicional</h2>
+      <h2>Ejercicio 7</h2>
       <Articulo isPublished={true} />
       <Articulo isPublished={false} />
     </div>
